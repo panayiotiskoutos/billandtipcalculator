f7dbc32829e139f1370391fdba5846c167908cec
Panayiotis Koutos <koutospanayiotis@gmail.com>
2025-07-27 01:44:25 +0300

    Add files via upload

---
+++ Bill and Tip.calculator.py
@@ -0,0 +1,8 @@
+    bill_amount = 100  # Example value
+    print("Bill Amount: $", bill_amount)
+    
+    tip_percentage = 10 / 100  # example value
+    print("Tip Percentage: ", tip_percentage * 100, "%")
+    
+    total_amount = bill_amount + (bill_amount * tip_percentage)
+    print("Total Amount: $", total_amount)
